68e2a0fb303e3dcfa4d27a4a5146a67e0de22513
RidhikaJoshi <ridhikasmrh@gmail.com>
2023-12-31 23:54:17 +0530

    Add heading to index.html and style changes

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 let n = Math.floor(Math.random() * quotes.length);
 let button = document.getElementById("button");
 let text = document.getElementById("text");
-text.innerHTML = quotes[n].quote;
-console.log(n);
-console.log(quotes[n].quote);
+let body = document.getElementsByTagName("body");
+text.innerHTML = `"${quotes[n].quote}"`;
+// console.log(n);
+// console.log(quotes[n].quote);
 
 button.onclick = () => {
 	let n = Math.floor(Math.random() * quotes.length);
-	text.innerHTML = `" ${quotes[n].quote}" `;
+	text.innerHTML = `"${quotes[n].quote}"`;
+	// body.style.backgroundimage = `url("https://source.unsplash.com/1600x900/?landscape")`;
 };
 // Auto- refreshes quotes in every 20 seconds
 setInterval(() => {
 	let n = Math.floor(Math.random() * quotes.length);
-	text.innerHTML = quotes[n].quote;
+	text.innerHTML = `"${quotes[n].quote}"`;
 }, 20000);
